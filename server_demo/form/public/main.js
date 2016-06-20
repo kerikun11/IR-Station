85f811553f65fcb31c38c48b123d29683a4c2e37
kerikun11 <kerikun11+github@gmail.com>
2016-06-20 20:03:43 +0900

    デモ用のSinatraサーバーを追加
    ruby sinatraが必要

---
+++ server_demo/form/public/main.js
@@ -0,0 +1,64 @@
+function getWifiList(){
+	console.log("Getting wifi list")
+	$.getJSON(
+		'/wifiList',
+		{},
+		function(data) {
+			$.each(data,function(index,wifi){
+				$('#wifiList').append($('<option>').val(wifi).text(wifi));
+			});
+			$('#info-status').text("Loading successful.")
+			$('#form').toggle();
+			$('#ap').toggle();
+		}
+	);
+}
+function form(){
+	if(confirm("Are you sure to confirm?")){
+		$('#form').toggle();
+		$('#ap').toggle();
+		$('#info-status').text("Connecting... Please wait.");
+		$.get('/confirm',{
+			ssid: $('#form [name="ssid"]').val(),
+			password: $('#form input[name="password"]').val(),
+			url: $('#form input[name="url"]').val()
+		}).done(function(res){
+			if(res=="false"){
+				$('#form').toggle();
+				$('#ap').toggle();
+				$('#info-status').text("Connection failed. Please try again.");
+			}else{
+				$('#info-status').text("Connection Successful.");
+			}
+		});
+	}
+}
+function setAP(){
+	if(confirm("Can I setup as Access Point Mode?")){
+		$('#form').toggle();
+		$('#ap').toggle();
+		$('#info-status').text("Connecting... Please wait.");
+		$.get('/accessPointMode',{
+			url: $('#ap input[name="url"]').val()
+		}).done(function(res){
+			$('#info-status').text(res);
+		});
+	}
+}
+
+$('#form button').click(form);
+$('#form input').keypress(function(e){
+	if(e.which == 13){
+		form();
+	}
+});
+
+$('#ap button').click(setAP);
+$('#ap input').keypress(function(e){
+	if(e.which == 13){
+		setAP();
+	}
+});
+
+getWifiList();
+
---
+++ server_demo/general/app.rb
@@ -0,0 +1,57 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'active_record'
+require 'sinatra/json'
+
+chName = [
+	"Light1 ON", "Light1 OFF", "Bright1", "Dark1", "Night1",
+	"Light2 ON", "Light2 OFF", "Bright2", "Dark2", "Night2",
+	"Light3 ON", "Light3 OFF", "Bright3", "Dark3", "Night3",
+	"Light4 ON", "Light4 OFF", "Bright4", "Dark4", "Night4",
+	"Light5 ON", "Light5 OFF", "Bright5", "Dark5", "Night5",
+]
+
+get '/' do
+	send_file File.join(settings.public_folder,'index.html')
+end
+
+get "/recode" do
+	sleep(2)
+	logger.info "ch = "+params[:ch]
+	logger.info "chName = "+params[:chName]
+	json "Recode a signal ch"+params[:ch]+" as "+chName[params[:ch].to_i-1]
+end
+
+get "/send" do
+	sleep(1)
+	logger.info "ch = "+params[:ch]
+	json "Sent a signal ch"+params[:ch]+": "+chName[params[:ch].to_i-1]
+end
+
+get "/clearAllSignals" do
+	sleep(2)
+	json "Cleared"
+end
+
+get "/disconnectWifi" do
+	sleep(1)
+	json "Disconnected"
+end
+
+get"/chName" do
+	sleep(1)
+	json chName
+end
+
+get"/status" do
+	sleep(1)
+	status = ["Listening..."]
+	json status
+end
+
+get"/info" do
+	sleep(1)
+	info = ["Listening...","Buffalo-G-46BB","192.168.11.8","http://esp8266.local"]
+	json info
+end
+
