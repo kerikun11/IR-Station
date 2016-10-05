19fac9593c6775246e01f0bd1b6884f0cb169745
kerikun11 <kerikun11+github@gmail.com>
2016-10-06 00:26:53 +0900

    Stringの文字数が長くても大丈夫なように修正
    もっと長くする必要があるかも．
    現状，50チャンネルは可能．

--- firmware/IR-station/file.h
+++ firmware/IR-station/file.h
@@ -12,8 +12,8 @@
 
 #include <ESP8266WiFi.h>
 
-bool writeStringToFile(String path, String dataString);
-bool getStringFromFile(String path, String &dataString);
+bool writeStringToFile(String path, String& dataString);
+bool getStringFromFile(String path, String& dataString);
 bool removeFile(String path);
 
 #endif
