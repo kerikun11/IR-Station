604dcfdea1fcc0329516592858a851ec9539673e
Ryotaro Onuki <kerikun11+github@gmail.com>
2020-04-07 09:28:39 +0900

    バージョン情報を更新 #17

--- firmware/IR-station/config.h
+++ firmware/IR-station/config.h
@@ -11,7 +11,7 @@
 #define __CONFIG_H__
 
 /* Version */
-#define IR_STATION_VERSION      "v1.5.4"
+#define IR_STATION_VERSION      "v1.6.0"
 
 /* Hardware Mapping */
 #define PIN_BUTTON              (0)
