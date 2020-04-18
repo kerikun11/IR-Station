fffec71d3527e7c517393fbabe1ee51a8802e8a7
Mizuho <mizuho128@gmail.com>
2020-04-18 23:20:44 +0900

    ws.rb UIデザイン円滑化のためのプロキシ

--- firmware/IR-station/webUI/ws.rb
+++ firmware/IR-station/webUI/ws.rb
@@ -1,3 +1,6 @@
+require 'net/http'
+require 'uri'
+
 require 'sinatra'
 
 pub = File.dirname(__FILE__) + "/main/"
@@ -7,9 +10,37 @@ get %r</([a-zA-Z\.]+)\.erb> do
   erb params["captures"].first.to_sym, locals: {alexa: true}
 end
 
-get '/info' do
-  content_type :json
+get %r{/([a-zA-Z-]+)(/[a-zA-Z-]+)?} do
+  api = params.delete("captures").join
+  uri = URI.parse("http://#{ARGV.first}/#{api}")
 
-  File.read(pub + "station.json")
+  req = Net::HTTP::Get.new(uri.path)
+  req.set_form_data(params)
+  res = Net::HTTP.start(uri.host, uri.port) do |http|
+      http.request(req)
+  end
+
+  puts "PROXY GET #{uri}, #{params.inspect}"
+  puts res.body
+
+  content_type res.content_type
+  res.body
 end
 
+post %r{/([a-zA-Z-]+)(/[a-zA-Z-]+)?} do
+  api = params.delete("captures").join
+  uri = URI.parse("http://#{ARGV.first}/#{api}")
+
+  req = Net::HTTP::Post.new(uri)
+  req.set_form_data(params)
+
+  res = Net::HTTP.start(uri.hostname, uri.port) do |http|
+      http.request(req)
+  end
+
+  puts "PROXY POST #{uri}, #{params.inspect}"
+  puts res.body
+
+  content_type res.content_type
+  res.body
+end
